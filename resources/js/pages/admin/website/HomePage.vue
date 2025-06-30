25b932a4b5c1b8bcd70515001ca203a4ddb35150
HazratBilal3537 <ba4407430@gmail.com>
2025-06-30 08:07:21 +0500

    complete aboute section in home

--- resources/js/pages/admin/website/HomePage.vue
+++ resources/js/pages/admin/website/HomePage.vue
@@ -85,7 +85,7 @@
             <Label for="devine_time">Divine Service</Label>
             <Input id="devine_time" type="text" v-model="form.devine_time" />
           </div>
-           <div class="flex-1 grid">
+          <div class="flex-1 grid">
             <Label for="side_img">Flag Icon</Label>
             <input id="flag_icon" type="file" accept="image/*" @change="handleflagiconChange"
               class="mt-2 block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100" />
@@ -118,49 +118,10 @@
             <Label :for="`card_button_url_${index}`">Button URL</Label>
             <Input :id="`card_button_url_${index}`" type="text" v-model="card.button_url" />
           </div>
-          <div class="flex-1 grid gap-2">
-            <Label :for="`card_icon_${index}`">Icon</Label>
-            <input :id="`card_icon_${index}`" type="file" accept="image/*" @change="handleFileUpload($event, index)"
-              class="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100" />
-            <div v-if="form.info_cards[index].icon_preview" class="mt-1">
-              <img :src="form.info_cards[index].icon_preview" alt="Icon Preview"
-                class="h-12 w-12 object-contain border rounded" />
-            </div>
-          </div>
+
         </div>
 
-        <!-- Upcoming Events -->
-        <Label>Upcoming Events</Label>
-        <div class="flex gap-4 flex-col md:flex-row">
-          <div class="flex-1 grid gap-2">
-            <Label for="event_title">Title</Label>
-            <Input id="event_title" type="text" v-model="form.upcoming_event_title" />
-            <InputError :message="form.errors.upcoming_event_title" />
-          </div>
-          <div class="flex-1 grid gap-2">
-            <Label for="event_description">Description</Label>
-            <textarea v-model="form.upcoming_event_description"
-              class="w-full h-24 rounded-md p-2 border border-gray-300"></textarea>
-            <InputError :message="form.errors.upcoming_event_description" />
-          </div>
-        </div>
-        <hr />
-        <!-- Church Calendar -->
-        <Label>Church Calendar</Label>
-        <div class="flex gap-4 flex-col md:flex-row">
-          <div class="flex-1 grid gap-2">
-            <Label for="calendar_title">Title</Label>
-            <Input id="calendar_title" type="text" v-model="form.calendar_title" />
-            <InputError :message="form.errors.calendar_title" />
-          </div>
-          <div class="flex-1 grid gap-2">
-            <Label for="calendar_description">Description</Label>
-            <textarea v-model="form.calendar_description"
-              class="w-full h-24 rounded-md p-2 border border-gray-300"></textarea>
-            <InputError :message="form.errors.calendar_description" />
-          </div>
-        </div>
-        <hr />
+
         <!-- Recurring Events -->
         <Label>Recurring Events</Label>
         <Label for="recurring_event_title">Title</Label>
@@ -223,6 +184,73 @@
           </div>
         </div>
         <hr />
+        <!-- About Shekinah Haitian SDA Church -->
+        <Label>About Section</Label>
+
+        <div class="flex gap-4 flex-col md:flex-row">
+          <div class="flex-1 grid gap-2">
+            <Label for="heading">Heading</Label>
+            <Input id="heading" type="text" v-model="form.about_heading" />
+            <InputError :message="form.errors.about_heading" />
+          </div>
+          <div class="flex-1 grid gap-2">
+            <Label for="name">Paragraph First</Label>
+            <textarea v-model="form.about_para_first"></textarea>
+            <InputError :message="form.errors.about_para_first" />
+          </div>
+          <div class="flex-1 grid gap-2">
+            <Label for="name">Paragraph Second</Label>
+            <textarea v-model="form.about_para_second"></textarea>
+            <InputError :message="form.errors.about_para_second" />
+          </div>
+        </div>
+        <div class="flex gap-4 flex-col md:flex-row">
+          <div class="flex-1 grid gap-2">
+            <Label for="heading">Our Mission Heading</Label>
+            <Input id="heading" type="text" v-model="form.mission_heading" />
+            <InputError :message="form.errors.mission_heading" />
+          </div>
+          <div class="flex-1 grid gap-2">
+            <Label for="name">Our Mission Description</Label>
+            <textarea v-model="form.mission_description"></textarea>
+            <InputError :message="form.errors.mission_description" />
+          </div>
+        </div>
+        <div class="flex gap-4 flex-col md:flex-row">
+          <div class="flex-1 grid gap-2">
+            <Label for="heading">Our Vision Heading</Label>
+            <Input id="heading" type="text" v-model="form.vision_heading" />
+            <InputError :message="form.errors.vision_heading" />
+          </div>
+          <div class="flex-1 grid gap-2">
+            <Label for="name">Our Vision Description</Label>
+            <textarea v-model="form.vision_description"></textarea>
+            <InputError :message="form.errors.vision_description" />
+          </div>
+        </div>
+        <div class="flex gap-4 flex-col md:flex-row">
+          <div class="flex-1 grid gap-2">
+            <Label for="heading">About Image 1</Label>
+            <Input id="about_image_1" type="file" @change="handleFileChange" />
+            <InputError :message="form.errors.about_image_1" />
+          </div>
+          <div class="flex-1 grid gap-2">
+            <Label for="heading">About Image 2</Label>
+            <Input id="about_image_2" type="file" @change="handleFileChange" />
+            <InputError :message="form.errors.about_image_2" />
+          </div>
+          <div class="flex-1 grid gap-2">
+            <Label for="heading">About Image 3</Label>
+            <Input id="about_image_3" type="file" @change="handleFileChange" />
+            <InputError :message="form.errors.about_image_3" />
+          </div>
+          <div class="flex-1 grid gap-2">
+            <Label for="heading">About Image 4</Label>
+            <Input id="about_image_4" type="file" @change="handleFileChange" />
+            <InputError :message="form.errors.about_image_4" />
+          </div>
+        </div>
+        <hr />
         <!-- Submit Button -->
         <div class="flex justify-end">
           <Button type="submit" class="text-end" :disabled="form.processing">
@@ -278,17 +306,12 @@ const form = useForm({
 
   // Info Cards section
   info_cards: props.data?.info_cards ?? [
-    { title: '', description: '', button_text: '', button_url: '', icon: null, icon_preview: null },
-    { title: '', description: '', button_text: '', button_url: '', icon: null, icon_preview: null },
-    { title: '', description: '', button_text: '', button_url: '', icon: null, icon_preview: null },
-    { title: '', description: '', button_text: '', button_url: '', icon: null, icon_preview: null },
+    { title: '', description: '', button_text: '', button_url: '' },
+    { title: '', description: '', button_text: '', button_url: '' },
+    { title: '', description: '', button_text: '', button_url: '' },
+    { title: '', description: '', button_text: '', button_url: '' },
   ],
-  // Upcoming events
-  upcoming_event_title: props.data?.upcoming_event_title ?? "",
-  upcoming_event_description: props.data?.upcoming_event_description ?? "",
-  // Church calendar
-  calendar_title: props.data?.calendar_title ?? "",
-  calendar_description: props.data?.calendar_description ?? "",
+
   // Recurring events
   recurring_event_title: props.data?.recurring_event_title ?? "",
   text_1: props.data?.text_1 ?? "",
@@ -301,23 +324,23 @@ const form = useForm({
   stay_connect_item_1: props.data?.stay_connect_item_1 ?? "",
   stay_connect_item_2: props.data?.stay_connect_item_2 ?? "",
   stay_connect_item_3: props.data?.stay_connect_item_3 ?? "",
-
+  // about shekinah
+  about_heading: props.data?.about_heading ?? "",
+  about_para_first: props.data?.about_para_first ?? "",
+  about_para_second: props.data?.about_para_second ?? "",
+  // mission and vision
+  mission_heading: props.data?.mission_heading ?? "",
+  mission_description: props.data?.mission_description ?? "",
+  vision_heading: props.data?.vision_heading ?? "",
+  vision_description: props.data?.vision_description ?? "",
+  // about images
+  about_image_1: props.data?.about_image_1 ?? "",
+  about_image_2: props.data?.about_image_2 ?? "",
+  about_image_3: props.data?.about_image_3 ?? "",
+  about_image_4: props.data?.about_image_4 ?? "",
 
 });
-const handleFileUpload = (event: Event, index: number) => {
-  const target = event.target as HTMLInputElement;
-  if (target.files && target.files[0]) {
-    const file = target.files[0];
-    form.info_cards[index].icon = file;
 
-    // Preview for user
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      form.info_cards[index].icon_preview = e.target?.result;
-    };
-    reader.readAsDataURL(file);
-  }
-};
 // Handle file change
 const handleSideImgChange = (e: Event) => {
   const file = (e.target as HTMLInputElement).files?.[0];
@@ -331,6 +354,28 @@ const handleflagiconChange = (e: Event) => {
   if (file) {
     form.flag_icon = file;
     previewflagicon.value = URL.createObjectURL(file);
+  }
+};
+
+// Handles file changes for about images
+const handleFileChange = (e: Event) => {
+  const input = e.target as HTMLInputElement;
+  const file = input.files?.[0];
+
+  if (!file) return;
+  switch (input.id) {
+    case "about_image_1":
+      form.about_image_1 = file;
+      break;
+    case "about_image_2":
+      form.about_image_2 = file;
+      break;
+    case "about_image_3":
+      form.about_image_3 = file;
+      break;
+    case "about_image_4":
+      form.about_image_4 = file;
+      break;
   }
 };
 
